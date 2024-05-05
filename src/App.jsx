4c019cd14fa1102785555bd264f6050a6a7e0009
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-05-05 01:38:03 +0000

    Added SpaceX images and set them as background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react'
+import fs from 'fs'
+
 function App() {
+  const [wallpaper, setWallpaper] = useState([])
+
+  // list of wallpapers
+  const wallpapers = [
+    'backgrounds/bg1.jpg',
+    'backgrounds/bg2.jpeg',
+    'backgrounds/bg3.jpeg',
+    'backgrounds/bg4.jpeg',
+    'backgrounds/bg5.jpeg',
+    'backgrounds/bg6.jpeg',
+    'backgrounds/bg7.jpeg',
+    'backgrounds/bg9.jpg',
+  ]
+
+
   return (
     <>
-      <div className="foundation">
+      <div
+        className="foundation"
+        style={{
+          backgroundImage: `url(${wallpapers[Math.floor(Math.random() * 9)]})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          height: '100vh',
+          width: '100vw',
+        }}
+      >
         <h3>WallyWally</h3>
         <p>0.1.2</p>
         <p>
